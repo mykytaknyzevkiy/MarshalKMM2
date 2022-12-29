00bb7d2e4b54d350d096e56cebf51006a4586beb
nek_zabirov <neka1dev@gmail.com>
2022-12-29 16:43:42 +0200

    Fix android map render

--- shared/src/androidMain/kotlin/com/yama/marshal/ui/view/IGoldMap.kt
+++ shared/src/androidMain/kotlin/com/yama/marshal/ui/view/IGoldMap.kt
@@ -1,19 +1,31 @@
 package com.yama.marshal.ui.view
 
+import android.graphics.Color
 import android.location.Location
+import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.viewinterop.AndroidView
 import co.touchlab.kermit.Logger
 import com.l1inc.viewer.Course3DRenderer
 import com.l1inc.viewer.Course3DRendererBase
 import com.l1inc.viewer.Course3DViewer
 import com.yama.marshal.tool.onEachList
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
+import kotlin.math.roundToInt
 
 @Composable
 internal actual fun IGoldMap(
@@ -29,32 +41,37 @@ internal actual fun IGoldMap(
             .apply {
                 layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
+                    ViewGroup.LayoutParams.MATCH_PARENT,
                 )
-                setIsCalloutOverlay(false)
-            }
-            .also { view ->
-                view.init(
-                    hashMapOf(
-                        //CourseGPSVectorDetailsRequest:vectorGPSObject
-                        Pair(renderData.idCourse, renderData.vectors),
-                        Pair(Course3DRenderer.COURSE_ID, renderData.idCourse)
-                    ),
-                    false,
-                    true,
-                    null,
-                    null,
-                    false
-                )
+                this.setIsCalloutOverlay(false)
             }
     }
 
     AndroidView(
         modifier = modifier,
-        factory = { mapView }
+        factory = {
+            FrameLayout(context).apply {
+                addView(mapView)
+            }
+        }
     )
 
     LaunchedEffect(context) {
+        mapView.onOrientationChanged()
+
+        mapView.init(
+            hashMapOf(
+                //CourseGPSVectorDetailsRequest:vectorGPSObject
+                Pair(renderData.idCourse, renderData.vectors),
+                Pair(Course3DRenderer.COURSE_ID, renderData.idCourse)
+            ),
+            false,
+            false,
+            null,
+            null,
+            false
+        )
+
         hole
             .filter {
                 it >= 0
@@ -63,6 +80,7 @@ internal actual fun IGoldMap(
                 Logger.i("IGoldMap", message = {
                     "Move to hole $it"
                 })
+
                 mapView.setCurrentHole(
                     it,
                     Course3DRendererBase.NavigationMode.NavigationMode2D,
