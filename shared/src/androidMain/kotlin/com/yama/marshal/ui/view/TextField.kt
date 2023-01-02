2c022f3c241bce831998771cdf2f4486b6995789
nek_zabirov <neka1dev@gmail.com>
2023-01-02 17:25:10 +0200

    Android text field

--- shared/src/androidMain/kotlin/com/yama/marshal/ui/view/TextField.kt
+++ shared/src/androidMain/kotlin/com/yama/marshal/ui/view/TextField.kt
@@ -1,13 +1,19 @@
 package com.yama.marshal.ui.view
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.VisualTransformation
+import com.yama.marshal.tool.closeKeyboard
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 internal actual fun TextField(
     value: String,
@@ -18,6 +24,8 @@ internal actual fun TextField(
     isEnable: Boolean,
     visualTransformation: VisualTransformation
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     OutlinedTextField(
         modifier = modifier,
         value = value,
@@ -26,6 +34,13 @@ internal actual fun TextField(
         },
         visualTransformation = visualTransformation,
         onValueChange = onValueChange,
-        isError = isError
+        isError = isError,
+        enabled = isEnable,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, autoCorrect = false),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                keyboardController?.hide()
+            }
+        ),
     )
 }
